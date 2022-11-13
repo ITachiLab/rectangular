872e4d61c1ef5a89ede2f0460ec494ce37033d9f
Itachi <contact@itachi.pl>
2022-11-13 20:02:58 +0100

    [GH-1] Implement macros for extracting lower and higher words sign-wise.
    In order to make sure the macros work correctly, unit tests have been implemented.

--- src/utils.rs
+++ src/utils.rs
@@ -12,12 +12,12 @@
 /// # Examples
 ///
 /// ```
-/// assert_eq!(rectangular::low_word!(0xDEADBEEF), 0xBEEF)
+/// assert_eq!(rectangular::low_word!(0xDEADBEEF), 0xBEEF);
 /// ```
 #[macro_export]
 macro_rules! low_word {
     ($x:expr) => {
-        $x & 0xFFFF
+        ($x as u32 & 0xFFFF) as u16
     }
 }
 
@@ -29,11 +29,47 @@ macro_rules! low_word {
 /// # Examples
 ///
 /// ```
-/// assert_eq!(rectangular::high_word!(0xDEADBEEF), 0xDEAD)
+/// assert_eq!(rectangular::high_word!(0xDEADBEEF), 0xDEAD);
 /// ```
 #[macro_export]
 macro_rules! high_word {
     ($x:expr) => {
-        loword!($x >> 16)
+        (($x as u32 >> 16) & 0xFFFF) as u16
+    }
+}
+
+/// Macro replacement for Windows API's `GET_X_LPARAM`.
+///
+/// This macro is similar to [`low_word!`] - it returns a lower 16 bits of the supplied number. The
+/// difference is the return type of the macro, this macro preserves sign of the lower word, thus
+/// making the return type `i16`.
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(rectangular::low_word_signed!(0x1234FFFF), -1);
+/// ```
+#[macro_export]
+macro_rules! low_word_signed {
+    ($x:expr) => {
+        ($x as u32 & 0xFFFF) as i16
+    }
+}
+
+/// Macro replacement for Windows API's `GET_Y_LPARAM`.
+///
+/// This macro is similar to [`high_word!`] - it returns a higher 16 bits of the supplied number.
+/// The difference is the return type of the macro, this macro preserves sign of the higher word,
+/// thus making the return type `i16`.
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(rectangular::high_word_signed!(0xFFFF1234), -1);
+/// ```
+#[macro_export]
+macro_rules! high_word_signed {
+    ($x:expr) => {
+        (($x as u32 >> 16) & 0xFFFF) as i16
     }
 }
