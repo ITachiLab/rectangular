97d9cd618d4284310bc48999ae555fc862d71dae
Itachi <contact@itachi.pl>
2022-11-28 01:25:13 +0100

    [GH-1] Refactor the code so the window procedure and rest of the code communicate nicer.
    The window creation and message processing has been moved to the RectangularWindow structure implementation.

--- src/context_menu.rs
+++ src/context_menu.rs
@@ -1,57 +1,90 @@
-//! This modules gathers functions related to any kind of interaction with context menus.
+//! This module contains implementation of a context menu.
+//!
+//! The context menu appears when a user clicks a notification icon with a secondary mouse button.
+//! The menu is the primary control center of the Rectangular application, as Rectangular does not
+//! have any windows.
 
 use windows::core::PCSTR;
 use windows::s;
-use windows::Win32::Foundation::{HWND, LPARAM, WPARAM};
-use windows::Win32::UI::WindowsAndMessaging::*;
+use windows::Win32::{
+    Foundation::*, UI::WindowsAndMessaging::*,
+};
 
 use crate::{high_word_signed, low_word_signed};
 
+/// An array of tuples, each representing a descriptor of the context menu item.
+///
+/// The tuple members are:
+/// 0. Menu item flags ([`MENU_ITEM_FLAGS`]).
+/// 1. Index of the menu entry, this will be used by `WM_COMMAND` message when user selects an
+/// entry.
+/// 2. Title of the entry.
 const MENU_ENTRIES: [(MENU_ITEM_FLAGS, usize, PCSTR); 1] = [
     (MF_STRING, 1, s!("Exit"))
 ];
 
+/// ContextMenu includes data and methods strictly related to the context menu of a notification
+/// icon.
 pub struct ContextMenu {
-    handle: HMENU,
-    owner: HWND,
+    menu_handle: HMENU,
+    window_handle: HWND,
+}
+
+impl Default for ContextMenu {
+    fn default() -> Self {
+        ContextMenu {
+            menu_handle: Default::default(),
+            window_handle: Default::default()
+        }
+    }
 }
 
 impl ContextMenu {
-    pub unsafe fn new(owner: HWND) -> Option<ContextMenu> {
-        let handle = CreatePopupMenu().ok()?;
-        let menu = ContextMenu { handle, owner };
+    /// Create a new context menu.
+    ///
+    /// Upon creating, all menu entries from [`MENU_ENTRIES`] are added to the menu.
+    ///
+    /// # Panics
+    ///
+    /// Panics if a call to [`CreatePopupMenu`] failed.
+    pub fn new(window_handle: HWND) -> ContextMenu {
+        unsafe {
+            let menu_handle = CreatePopupMenu().expect("Could not create popup menu.");
 
-        for (flags, id, title) in &MENU_ENTRIES {
-            AppendMenuA(handle, *flags, *id, *title);
+            for (flags, id, title) in &MENU_ENTRIES {
+                AppendMenuA(menu_handle, *flags, *id, *title);
+            }
+
+            ContextMenu { menu_handle, window_handle }
         }
-
-        Some(menu)
     }
 
-    pub unsafe fn from_window(owner: HWND) -> *const ContextMenu {
-        GetWindowLongPtrA(owner, WINDOW_LONG_PTR_INDEX(0)) as *const ContextMenu
-    }
-
-    pub unsafe fn bind_to_window(&self) {
-        let ptr = self as *const ContextMenu;
-        SetWindowLongPtrA(self.owner, WINDOW_LONG_PTR_INDEX(0), ptr as isize);
-    }
-
-    pub unsafe fn show(&self, wparam: WPARAM) {
+    /// Show the context menu.
+    ///
+    /// This method extracts X and Y position of the cursor from the supplied `WPARAM` and uses
+    /// these coordinates to show a context menu.
+    ///
+    /// The method follows the recommended way of showing menus in order to avoid glitches:
+    /// [TrackPopupMenu#Remarks](https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-trackpopupmenu#remarks).
+    pub fn show(&self, wparam: WPARAM) -> LRESULT {
         let x = low_word_signed!(wparam.0);
         let y = high_word_signed!(wparam.0);
 
-        SetForegroundWindow(self.owner);
-        TrackPopupMenu(self.handle, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_BOTTOMALIGN, x as i32,
-                       y as i32, 0, self.owner, None);
-        PostMessageA(self.owner, WM_NULL, WPARAM(0), LPARAM(0));
+        unsafe {
+            SetForegroundWindow(self.window_handle);
+            TrackPopupMenu(self.menu_handle, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_BOTTOMALIGN,
+                           x as i32, y as i32, 0, self.window_handle, None);
+            PostMessageA(self.window_handle, WM_NULL, WPARAM(0), LPARAM(0));
+        }
+
+        LRESULT(0)
     }
 }
 
 impl Drop for ContextMenu {
     fn drop(&mut self) {
         unsafe {
-            DestroyMenu(self.handle);
+            DestroyMenu(self.menu_handle);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,11 @@
 #![windows_subsystem = "windows"]
 
-use windows::{
-    core::*, Win32::Foundation::*, Win32::UI::WindowsAndMessaging::*,
-    Win32::System::LibraryLoader::GetModuleHandleA
-};
-
-use rectangular::{
-    notification, low_word
-};
-
-use rectangular::context_menu::ContextMenu;
-
-const WINDOW_CLASS_NAME: PCSTR = s!("main");
-const WINDOW_NAME: PCSTR = s!("Rectangular");
+use windows::core::Result;
+use rectangular::rectangular_window::RectangularWindow;
 
 fn main() -> Result<()> {
-    unsafe {
-        let instance = GetModuleHandleA(None)?;
-
-        let wc = WNDCLASSA {
-            style: Default::default(),
-            lpfnWndProc: Some(wndproc),
-            hInstance: instance,
-            lpszClassName: WINDOW_CLASS_NAME,
-            cbWndExtra: ((isize::BITS / 8) * 2) as i32,
-            ..Default::default()
-        };
-
-        RegisterClassA(&wc);
-
-        let window_handle = CreateWindowExA(
-            WINDOW_EX_STYLE::default(),
-            WINDOW_CLASS_NAME,
-            WINDOW_NAME,
-            WINDOW_STYLE::default(),
-            0,
-            0,
-            0,
-            0,
-            HWND_MESSAGE,
-            None,
-            instance,
-            None,
-        );
-
-        notification::add_notification_icon(instance, window_handle);
-
-        let menu = ContextMenu::new(window_handle)
-            .expect("Could not create context menu.");
-        menu.bind_to_window();
-
-        let mut message = MSG::default();
-        while GetMessageA(&mut message, HWND(0), 0, 0).into() {
-            TranslateMessage(&message);
-            DispatchMessageA(&message);
-        }
-    }
+    let rectangular_window: Box<RectangularWindow> = RectangularWindow::new();
+    (*rectangular_window).enter_loop();
 
     Ok(())
-}
-
-extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    unsafe {
-        match message {
-            WM_DESTROY => {
-                notification::delete_notification_icon(window);
-                PostQuitMessage(0);
-                LRESULT(0)
-            },
-            WM_COMMAND => {
-                PostMessageA(window, WM_CLOSE, WPARAM(0), LPARAM(0));
-                LRESULT(0)
-            },
-            notification::CALLBACK_MESSAGE => {
-                match low_word!(lparam.0) as u32 {
-                    WM_CONTEXTMENU => {
-                        let menu = ContextMenu::from_window(window);
-                        (*menu).show(wparam);
-                        LRESULT(0)
-                    },
-                    _ => DefWindowProcA(window, message, wparam, lparam)
-                }
-            },
-            _ => DefWindowProcA(window, message, wparam, lparam),
-        }
-    }
 }
