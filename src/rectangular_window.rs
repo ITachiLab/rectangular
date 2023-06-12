e353c57ecaac397f2c8f035114a37542cdd101d1
Itachi <contact@itachi.pl>
2023-06-12 08:27:41 +0200

    [GH-2] Add "Control Center" to the main window's structure. Adapt the main window implementation to the new mechanism of creating and maintaining windows.

--- src/rectangular_window.rs
+++ src/rectangular_window.rs
@@ -1,27 +1,25 @@
 //! This module is dedicated to main window.
 //!
-//! [`RectangularWindow`] structure is the central component of the application. It is responsible
-//! for instantiation and initialization of other components, and also for dispatching proper
-//! window messages to them.
+//! [`RectangularWindow`] implements behaviour of the main application window which is a so-called
+//! "message-only window".
 
+use std::cell::RefCell;
 use std::ffi::c_void;
-use std::ptr::{null};
+use std::rc::Rc;
+
 use windows::core::PCSTR;
 use windows::s;
 use windows::Win32::Foundation::{HINSTANCE, HWND, LPARAM, LRESULT, WPARAM};
-use windows::Win32::System::LibraryLoader::GetModuleHandleA;
 use windows::Win32::UI::WindowsAndMessaging::*;
 
+use crate::app_window::AppWindow;
 use crate::context_menu::ContextMenu;
-use crate::low_word;
+use crate::control_center::ControlCenter;
+use crate::{low_word, WINDOW_CLASS_NAME, WM_NIACTION};
 use crate::notification::NotificationIcon;
 
-/// An ID of the message which will be used to communicate with the main window's message loop.
-pub const WM_NIACTION: u32 = WM_USER + 1;
-
-const WINDOW_CLASS_NAME: PCSTR = s!("main");
+/// A name of the main application window.
 const WINDOW_NAME: PCSTR = s!("Rectangular");
-const WINDOW_EXTRAS_MAIN: WINDOW_LONG_PTR_INDEX = WINDOW_LONG_PTR_INDEX(0);
 
 /// This structure represents the main application window.
 ///
@@ -34,6 +32,7 @@ pub struct RectangularWindow {
 
     pub context_menu: ContextMenu,
     pub notification_icon: NotificationIcon,
+    pub control_center: Rc<RefCell<ControlCenter>>,
 }
 
 impl Default for RectangularWindow {
@@ -42,145 +41,72 @@ impl Default for RectangularWindow {
             window_handle: Default::default(),
             context_menu: Default::default(),
             notification_icon: Default::default(),
+            control_center: Default::default(),
         }
     }
 }
 
+impl AppWindow for RectangularWindow {
+    fn handle_create(&mut self, app_instance: HINSTANCE, window: HWND, _message: u32,
+                     _wparam: WPARAM, _lparam: LPARAM) -> LRESULT {
+        self.window_handle = window;
+
+        self.notification_icon = NotificationIcon::new(window, app_instance);
+        self.notification_icon.add_to_window();
+
+        self.context_menu = ContextMenu::new(window);
+
+        self.control_center = ControlCenter::new(app_instance);
+
+        LRESULT(0)
+    }
+
+    fn process_message(&self, _window: HWND, message: u32, wparam: WPARAM,
+                       lparam: LPARAM) -> LRESULT {
+        match message {
+            WM_DESTROY => {
+                unsafe { PostQuitMessage(0); }
+            },
+            WM_COMMAND => {
+                unsafe { PostMessageA(self.window_handle, WM_CLOSE, WPARAM(0), LPARAM(0)); }
+            },
+            WM_NIACTION => match low_word!(lparam.0) as u32 {
+                WM_CONTEXTMENU => {
+                    self.context_menu.show(wparam);
+                },
+                WM_LBUTTONUP => {
+                    self.control_center.borrow().show(wparam);
+                },
+                _ => unsafe { return DefWindowProcA(self.window_handle, message, wparam, lparam); }
+            },
+            _ => unsafe { return DefWindowProcA(self.window_handle, message, wparam, lparam); }
+        }
+
+        LRESULT(0)
+    }
+}
+
 impl RectangularWindow {
-    /// Create a new system window and a [`RectangularWindow`] for keeping the components together.
-    ///
-    /// The method prepares and creates a new system window for Rectangular. It also takes care of
-    /// instantiating [`RectangularWindow`] structure and passing it properly to the window
-    /// procedure for initialization during the `WM_CREATE` message.
-    ///
-    /// The return value is a boxed [`RectangularWindow`]. The reason for returning a [`Box`] is a
-    /// necessity of storing a pointer to [`RectangularWindow`] instance within a window memory
-    /// space, hence the instance cannot live on the stack because it will be invalidated soon after
-    /// the `new()` method finishes. Thanks to boxing, the instance lives on the heap and the box
-    /// can be safely moved out of the method, while leaving the wrapped instance intact.
-    pub fn new() -> Box<RectangularWindow> {
-        let mut window_box = Box::new(RectangularWindow::default());
-        let window_ptr: *mut RectangularWindow = &mut *window_box;
+    /// Create a new system window and a [`RectangularWindow`] instance.
+    pub fn new(instance: HINSTANCE) -> Rc<RefCell<RectangularWindow>> {
+        let my_rc = Rc::new(RefCell::new(RectangularWindow::default()));
+        let boxed = Box::new(Rc::clone(&my_rc) as Rc<RefCell<dyn AppWindow>>);
+        let raw = Box::into_raw(boxed);
 
         unsafe {
-            let instance = GetModuleHandleA(None)
-                .expect("Could not get handle of the application's module.");
-
-            let wc = WNDCLASSA {
-                style: Default::default(),
-                lpfnWndProc: Some(wndproc),
-                hInstance: instance,
-                lpszClassName: WINDOW_CLASS_NAME,
-                cbWndExtra: (isize::BITS / 8) as i32,
-                ..Default::default()
-            };
-
-            RegisterClassA(&wc);
             CreateWindowExA(
                 WINDOW_EX_STYLE::default(),
                 WINDOW_CLASS_NAME,
                 WINDOW_NAME,
                 WINDOW_STYLE::default(),
-                0,
-                0,
-                0,
-                0,
+                0, 0, 0, 0,
                 HWND_MESSAGE,
                 None,
                 instance,
-                Some(window_ptr as *mut c_void),
+                Some(raw as *const Rc<RefCell<dyn AppWindow>> as *const c_void),
             );
-
-            window_box
         }
-    }
 
-    /// Enter the window message loop.
-    ///
-    /// Call to this function never returns until the application is going to be closed.
-    pub fn enter_loop(&self) {
-        let mut message = MSG::default();
-
-        unsafe {
-            while GetMessageA(&mut message, HWND(0), 0, 0).into() {
-                TranslateMessage(&message);
-                DispatchMessageA(&message);
-            }
-        }
-    }
-
-    /// Process a windows message.
-    ///
-    /// This is the main handler of windows messages, it is called from inside the window procedure.
-    /// The handler dispatches messages to appropriate methods of either this class or underlying
-    /// components, in particular: context menu and notification icon.
-    fn process_message(&self, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-        match message {
-            WM_DESTROY => self.handle_destroy(),
-            WM_COMMAND => self.handle_command(),
-            WM_NIACTION => match low_word!(lparam.0) as u32 {
-                WM_CONTEXTMENU => self.context_menu.show(wparam),
-                _ => unsafe { DefWindowProcA(self.window_handle, message, wparam, lparam) }
-            }
-            _ => unsafe { DefWindowProcA(self.window_handle, message, wparam, lparam) }
-        }
-    }
-
-    /// Handler of `WM_CREATE` message.
-    ///
-    /// In contrast to other handlers, this handler is not called from
-    /// [`RectangularWindow::process_message`] method because the instance is not yet fully
-    /// initialized. This method is called automatically only once from the window procedure.
-    fn handle_create(&mut self, window_handle: HWND, app_instance: HINSTANCE) -> LRESULT {
-        self.window_handle = window_handle;
-
-        self.notification_icon = NotificationIcon::new(window_handle, app_instance);
-        self.notification_icon.add_to_window();
-
-        self.context_menu = ContextMenu::new(window_handle);
-
-        LRESULT(0)
-    }
-
-    /// Handler of `WM_COMMAND` message.
-    fn handle_command(&self) -> LRESULT {
-        unsafe { PostMessageA(self.window_handle, WM_CLOSE, WPARAM(0), LPARAM(0)); }
-        LRESULT(0)
-    }
-
-    /// Handler of `WM_DESTROY` message.
-    ///
-    /// This is a perfect place to do any cleanup actions before the application closes. Anything
-    /// that is not covered by [`Drop`] trait must be "dropped" here.
-    fn handle_destroy(&self) -> LRESULT {
-        unsafe { PostQuitMessage(0); }
-        LRESULT(0)
-    }
-}
-
-/// This is a window procedure, the glue between Windows and the application.
-///
-/// The code within the procedure is limited to the necessary minimum. Message processing and other
-/// logic lives inside [`RectangularWindow`] instance.
-extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    unsafe {
-        if message == WM_CREATE {
-            let create_struct = lparam.0 as *const CREATESTRUCTA;
-            let rectangular_window = (*create_struct).lpCreateParams as *mut RectangularWindow;
-
-            SetWindowLongPtrA(window, WINDOW_EXTRAS_MAIN, rectangular_window as isize);
-
-            (*rectangular_window).handle_create(window, (*create_struct).hInstance)
-        } else {
-            let rectangular_window =
-                GetWindowLongPtrA(window, WINDOW_EXTRAS_MAIN) as *const RectangularWindow;
-
-            // Some messages are sent before the RectangularWindow is instantiated.
-            if rectangular_window != null() {
-                (*rectangular_window).process_message(message, wparam, lparam)
-            } else {
-                DefWindowProcA(window, message, wparam, lparam)
-            }
-        }
+        my_rc
     }
 }
